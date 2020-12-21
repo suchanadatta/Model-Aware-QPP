884773e52c1442d3b708653134f1fad9e092bbdf
Suchana Datta <suchanadatta@gmail.com>
2020-12-21 21:29:52 +0530

    first commit

---
+++ src/main/java/org/neuralqpp/knrm/CreateMatrixFromPrerank.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.neuralqpp.knrm;
+
+/**
+ *
+ * @author suchana
+ */
+public class CreateMatrixFromPrerank {
+    
+}
